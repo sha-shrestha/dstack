ce80ee77faaddc09755b70683e4cddbdf9439fc1
peterschmidt85 <andrey.cheptsov@gmail.com>
2020-09-04 11:35:36 +0200

    Analytics add remoteAddr

--- server-base/src/main/kotlin/ai/dstack/server/services/AnalyticsService.kt
+++ server-base/src/main/kotlin/ai/dstack/server/services/AnalyticsService.kt
@@ -1,5 +1,9 @@
 package ai.dstack.server.services
 
 interface AnalyticsService {
+    fun track(category: String, action: String, label: String? = null, source: Any? = null, remoteAddr: String?) =
+            track(category, action, label, source)
+
+    @Deprecated("Use track with remoteAddr")
     fun track(category: String, action: String, label: String? = null, source: Any? = null)
 }
