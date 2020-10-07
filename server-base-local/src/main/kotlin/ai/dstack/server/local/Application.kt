d4cb969abd07989264e25e537ba040c8bea56655
peterschmidt85 <andrey.cheptsov@gmail.com>
2020-10-07 20:57:59 +0200

    Moving SQLite from "server-local-cli" to "server-base-local" (explude cassandra from auto-configuration - reverted)

--- server-base-local/src/main/kotlin/ai/dstack/server/local/Application.kt
+++ server-base-local/src/main/kotlin/ai/dstack/server/local/Application.kt
@@ -1,12 +1,10 @@
 package ai.dstack.server.local
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.data.cassandra.CassandraDataAutoConfiguration
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @EnableScheduling
-@SpringBootApplication(exclude = [CassandraDataAutoConfiguration::class])
+@SpringBootApplication
 @ComponentScan("ai.dstack.server")
 open class Application
