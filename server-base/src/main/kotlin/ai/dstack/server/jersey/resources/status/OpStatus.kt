59365fc2dce822b0e81f6383b8ffd43d7316c3f4
peterschmidt85 <andrey.cheptsov@gmail.com>
2020-11-06 09:21:22 +0100

    Major change: Include null values (e.g. Stack.readme, Dashboard.description, etc) in objects returned by API. (bugfix)

--- server-base/src/main/kotlin/ai/dstack/server/jersey/resources/status/OpStatus.kt
+++ server-base/src/main/kotlin/ai/dstack/server/jersey/resources/status/OpStatus.kt
@@ -1,3 +1,6 @@
 package ai.dstack.server.jersey.resources.status
 
+import com.fasterxml.jackson.annotation.JsonInclude
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 data class OpStatus(val message: String? = null)
