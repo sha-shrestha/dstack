782b4f3e1c0ca481139f139e7858c5c6fcb24b30
olgenn <vavilovolegik@gmail.com>
2020-11-18 15:53:15 +0300

    update package version

--- dstack-react/dist/index.js
+++ dstack-react/dist/index.js
@@ -262,7 +262,8 @@ var config = {
   BLOG_URL: 'https://blog.dstack.ai',
   TOKEN_STORAGE_KEY: 'token'
 };
-var reportPlotPythonCode = "import matplotlib.pyplot as plt\nimport dstack as ds\n\nfig = plt.figure()\nplt.plot([1, 2, 3, 4], [1, 4, 9, 16])\n\nds.push_frame(\"simple\", fig, \"My first plot\")";
+var reportModelPythonCode = "import dstack as ds\nfrom sklearn.linear_model import LogisticRegression\n\n# Load and prepare data (X_train, y_train)\n\nmodel = LogisticRegression()\nmodel.fit(X_train, y_train)\n\nds.push(\"my_model\", model, \"My first linear model\")";
+var reportPlotPythonCode = "import matplotlib.pyplot as plt\nimport dstack as ds\n\nfig = plt.figure()\nplt.plot([1, 2, 3, 4], [1, 4, 9, 16])\n\nds.push_frame(\"my_plot\", fig, \"My first plot\")";
 
 var apiFabric = function apiFabric(_temp) {
   var _ref = _temp === void 0 ? {} : _temp,
@@ -4437,7 +4438,7 @@ var UploadStack = function UploadStack(_ref) {
   }), activePublishTab === 1 && /*#__PURE__*/React__default.createElement(CodeViewer, {
     className: css$E.code,
     language: "python"
-  }, reportPlotPythonCode), activePublishTab === 2 && /*#__PURE__*/React__default.createElement(CodeViewer, {
+  }, reportModelPythonCode), activePublishTab === 2 && /*#__PURE__*/React__default.createElement(CodeViewer, {
     className: css$E.code,
     language: "python"
   }, reportPlotPythonCode)), activeCodeTab === 3 && /*#__PURE__*/React__default.createElement(Upload$1, {
