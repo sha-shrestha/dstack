54f3fce34ca38f9788926119767c977cf2325628
sha-shrestha <1738675+sha-shrestha@users.noreply.github.com>
2020-12-26 20:44:30 +0000

    Addressing PR comments (2)

--- docker/base-image/build.sh
+++ docker/base-image/build.sh
@@ -4,7 +4,4 @@ PARENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 source $PARENT_DIR/variables.sh
 
-echo Logging in to Docker as user $DOCKER_USER
-$DOCKER login --username "$DOCKER_USER" --password "$DOCKER_PASS" $DOCKER_REGISTRY
-
 $DOCKER build --progress plain --tag $IMAGE_NAME:$LATEST_TAG -f $PARENT_DIR/Dockerfile $PARENT_DIR
