3dcbea22e3cc3b9535280db7d3520a5bd8cdc030
sha-shrestha <1738675+sha-shrestha@users.noreply.github.com>
2020-12-26 17:05:45 +0000

    Fixing workflows  yaml

--- docker/base-image/build.sh
+++ docker/base-image/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-source variables.sh
+PARENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+source $PARENT_DIR/variables.sh
 
 $DOCKER build --progress plain --tag $IMAGE_NAME:$LATEST_TAG .
 $DOCKER tag $IMAGE_NAME:$LATEST_TAG $REGISTRY_IMAGE_FULL_PATH
